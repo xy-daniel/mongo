7d4ba9551b3bf4259f183b392f20916ca75d4365
p4m4p <myarctic@163.com>
2020-03-14 01:13:21 +0800

    自定义查询 MongoTemplate

--- src/main/java/org/javaboy/mongo/dao/BookDao.java
+++ src/main/java/org/javaboy/mongo/dao/BookDao.java
@@ -3,6 +3,9 @@ package org.javaboy.mongo.dao;
 import org.javaboy.mongo.bean.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BookDao extends MongoRepository<Book, Integer> {
+import java.util.List;
 
+public interface BookDao extends MongoRepository<Book, Integer> {
+    //仿造Jpa
+    List<Book> findBookByNameContaining(String name);
 }
--- src/test/java/org/javaboy/mongo/MongoApplicationTests.java
+++ src/test/java/org/javaboy/mongo/MongoApplicationTests.java
@@ -5,6 +5,7 @@ import org.javaboy.mongo.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.List;
 
@@ -32,6 +33,25 @@ class MongoApplicationTests {
     void select() {
         List<Book> all = bookDao.findAll();
         System.out.println(all);
+    }
+
+    @Test
+    void findBookByNameContaining(){
+        List<Book> name = bookDao.findBookByNameContaining("name");
+        System.out.println(name);
+    }
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Test
+    void test(){
+        Book book = new Book();
+        book.setId(3);
+        book.setName("红楼梦");
+        book.setAuthor("曹雪芹");
+        mongoTemplate.insert(book);
+        System.out.println(mongoTemplate.findAll(Book.class));;
     }
 
 }
